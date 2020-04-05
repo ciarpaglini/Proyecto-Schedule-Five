ab3b5ceda1f1060446c3171db988e3c590f7da93
Herman Martinez <60677357+ciarpaglini@users.noreply.github.com>
2020-04-05 16:45:57 +0200

    Update db.php

--- schedule/config/db.php
+++ schedule/config/db.php
@@ -2,7 +2,7 @@
 
 class Database{
 	public static function connect(){
-		$db = new mysqli('localhost', 'ciarpaglini', 'Austin.57', 'schedule_five');
+		$db = new mysqli('localhost', 'xxxxxx', 'xxxxxx', 'xxxxxxx');
 		$db->query("SET NAMES 'utf8'");
 		return $db;
 	}
